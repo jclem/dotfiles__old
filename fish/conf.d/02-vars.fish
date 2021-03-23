9f6c019914eb726457fa5c3f4499f830ec1bc0f9
Jonathan Clem <jonathan@jclem.net>
2021-03-23 19:16:44 -0400

    Add git info to shell prompt

--- fish/conf.d/02-vars.fish
+++ fish/conf.d/02-vars.fish
@@ -2,3 +2,9 @@ set -x CODE "$HOME/src/github.com"
 set -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 2097152"
 set -x GPG_TTY (tty)
 set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x __fish_git_prompt_showcolorhints true
+set -x __fish_git_prompt_use_informative_chars true
+set -x __fish_git_prompt_color brblack
+set -x __fish_git_prompt_color_prefix brblack
+set -x __fish_git_prompt_color_suffix brblack
+set -x __fish_git_prompt_char_stateseparator ' → '
