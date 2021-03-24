04496fde025bf113c9a52d2cec185858df0bc0ee
Jonathan Clem <jonathan@jclem.net>
2021-03-24 12:57:00 -0400

    Add tmux

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ dotfiles_dir="$(pwd)"
 
 ln -fsv "$dotfiles_dir/fish" "$HOME/.config"
 ln -fsv "$dotfiles_dir/git" "$HOME/.config"
+ln -fsv "$dotfiles_dir/tmux" "$HOME/.config"
 
 if test -f "$dotfiles_dir/git/local"; then
     ln -fsv "$dotfiles_dir/git/local" "$HOME/.gitconfig_local"
