3a440801a835010c3da07e26e295bb13a7ba9678
Jonathan Clem <jonathan@jclem.net>
2021-03-30 13:31:32 -0400

    Add gh config

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ ln -fsv "$dotfiles_dir/fish" "$HOME/.config"
 ln -fsv "$dotfiles_dir/git" "$HOME/.config"
 ln -fsv "$dotfiles_dir/tmux" "$HOME/.config"
 
+mkdir -p "$HOME/.config/gh"
+ln -fsv "$dotfiles_dir/gh/config.yml" "$HOME/.config/gh/config.yml"
+
 if test -f "$dotfiles_dir/git/local"; then
     ln -fsv "$dotfiles_dir/git/local" "$HOME/.gitconfig_local"
 fi
