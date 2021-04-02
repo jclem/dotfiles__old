076a7319b2e691f3058fed03485c4d23f7da66db
Jonathan Clem <jonathan@jclem.net>
2021-04-02 15:56:31 -0400

    Add vercel abbr

--- fish/conf.d/00-abbreviations.fish
+++ fish/conf.d/00-abbreviations.fish
@@ -3,7 +3,8 @@
 if status --is-interactive
     abbr --add --global dc docker-compose
     abbr --add --global dk docker
-    abbr --add --global g git
+    abbr --add --global g  git
     abbr --add --global hk heroku
     abbr --add --global ls exa
+    abbr --add --global v  vercel
 end
