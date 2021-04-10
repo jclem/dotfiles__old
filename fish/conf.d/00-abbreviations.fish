5fc9dd6f3d66f09a206a186fa2fa815cb6fa8035
Jonathan Clem <jonathan@jclem.net>
2021-04-10 15:16:22 -0400

    Switch back to docker-compose because bugs

--- fish/conf.d/00-abbreviations.fish
+++ fish/conf.d/00-abbreviations.fish
@@ -1,7 +1,7 @@
 # See: https://fishshell.com/docs/current/commands.html#abbr
 
 if status --is-interactive
-    abbr --add --global dc docker compose
+    abbr --add --global dc docker-compose
     abbr --add --global dk docker
     abbr --add --global g git
     abbr --add --global hk heroku
