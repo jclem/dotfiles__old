6ba746902de3fa66ee8a3ecb6301223a80ce60e3
Jonathan Clem <jonathan@jclem.net>
2021-04-14 12:14:47 -0400

    Handle no script in ns function

--- fish/functions/ns.fish
+++ fish/functions/ns.fish
@@ -3,5 +3,12 @@ function ns
         return 1
     end
 
-    npm run (cat package.json | jq -r '.scripts | to_entries[] | "\(.key): \(.value)"' | fzf --query "$argv[1]" | cut -d: -f1)
+
+    set script (cat package.json | jq -r '.scripts | to_entries[] | "\(.key): \(.value)"' | fzf --query "$argv[1]" | cut -d: -f1)
+
+    if [ -z "$script" ]
+        return 1
+    end
+
+    npm run $script
 end
