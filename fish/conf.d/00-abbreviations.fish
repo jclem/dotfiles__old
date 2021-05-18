9190b04848beeb4e64c7a4f4d2119e226bd77d3f
Jonathan Clem <jonathan@jclem.net>
2021-05-18 16:14:30 -0400

    Use code-insiders

--- fish/conf.d/00-abbreviations.fish
+++ fish/conf.d/00-abbreviations.fish
@@ -1,6 +1,7 @@
 # See: https://fishshell.com/docs/current/commands.html#abbr
 
 if status --is-interactive
+    abbr --add --global code code-insiders
     abbr --add --global dc docker-compose
     abbr --add --global dk docker
     abbr --add --global g git
