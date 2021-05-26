facd216b3a45f1daa8eac33a3f8d14b4829e12e5
Jonathan Clem <jonathan@jclem.net>
2021-05-26 13:56:23 +0000

    Set shell for both root and codespace users in install-codespaces.sh

--- install-codespaces.sh
+++ install-codespaces.sh
@@ -4,4 +4,5 @@ CI=true curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/insta
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 brew install bat exa fd fish fzf httpie jq ripgrep wget
 chsh -s (brew --prefix fish) root
+chsh -s (brew --prefix fish) codespace
 curl -sL https://git.io/fisher | source && fisher update
