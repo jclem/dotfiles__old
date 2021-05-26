751cf1dcb0737840a37d1af331374eb23e7d342b
Jonathan Clem <jonathan@jclem.net>
2021-05-26 13:42:39 +0000

    Properly run Homebrew install for Linux

--- install-codespaces.sh
+++ install-codespaces.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-CI=true /bin/bash -c (curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+CI=true curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /bin/bash
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 brew install bat exa fd fish fzf httpie jq ripgrep wget
 chsh -s (brew --prefix fish) root
