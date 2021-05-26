cebddc76ad5e7e60b2c3944acf603cceafbb5466
Jonathan Clem <jonathan@jclem.net>
2021-05-26 14:17:09 +0000

    Use hack to set shell for Codespaces

--- install-codespaces.sh
+++ install-codespaces.sh
@@ -3,6 +3,7 @@
 CI=true curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /bin/bash
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 brew install bat exa fd fish fzf httpie jq ripgrep wget
-chsh -s (brew --prefix fish) root
-chsh -s (brew --prefix fish) codespace
 curl -sL https://git.io/fisher | source && fisher update
+
+# Hack for Codespaces to get Fish as our default shell.
+echo "exec "(which fish) > ~/.bashrc
