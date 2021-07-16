4aa91aec8b41826f174000e094e129c243cd7f9d
Jonathan Clem <jonathan@jclem.net>
2021-07-16 14:53:42 -0400

    Disable code-insiders abbreviation

--- fish/conf.d/00-abbreviations.fish
+++ fish/conf.d/00-abbreviations.fish
@@ -1,7 +1,6 @@
 # See: https://fishshell.com/docs/current/commands.html#abbr
 
 if status --is-interactive
-    abbr --add --global code code-insiders
     abbr --add --global dc docker-compose
     abbr --add --global dk docker
     abbr --add --global g git
