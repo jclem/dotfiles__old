d8f33e4cbe4f422596522c803f13aa7967a7aff2
Jonathan Clem <jonathan@jclem.net>
2021-08-24 18:11:11 +0000

    Fix install-codespaces to run in bash

--- install-codespaces.sh
+++ install-codespaces.sh
@@ -3,7 +3,10 @@
 CI=true curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /bin/bash
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install bat exa fd fish fzf gh httpie jq ripgrep wget
-curl -sL https://git.io/fisher | source && fisher update
+fish -c 'curl -sL https://git.io/fisher | source && fisher update'
 
-# Hack for Codespaces to get Fish as our default shell.
-echo "exec $(which fish)" > ~/.bashrc
+# Add Homebrew fish to shells
+echo "$(which fish)" | sudo tee -a /etc/shells
+
+# Set the shell
+sudo chsh -s $(which fish) $(whoami)
