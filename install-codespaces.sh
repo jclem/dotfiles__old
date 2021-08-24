c234697e841aab1db67328474b8744d062d1c2c0
Jonathan Clem <jonathan@jclem.net>
2021-08-24 15:34:17 +0000

    Install gh with in Codespaces

--- install-codespaces.sh
+++ install-codespaces.sh
@@ -2,7 +2,7 @@
 
 CI=true curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /bin/bash
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew install bat exa fd fish fzf httpie jq ripgrep wget
+brew install bat exa fd fish fzf gh httpie jq ripgrep wget
 curl -sL https://git.io/fisher | source && fisher update
 
 # Hack for Codespaces to get Fish as our default shell.
