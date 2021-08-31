f17f8ac041341f1e0ccbe13ab7153442bd1f4013
Jonathan Clem <jonathan@jclem.net>
2021-08-31 12:13:39 -0400

    Add asdf to install-codespaces.sh brew formulae

--- install-codespaces.sh
+++ install-codespaces.sh
@@ -2,7 +2,7 @@
 
 CI=true curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /bin/bash
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew install bat exa fd fish fzf gh httpie jq ripgrep wget
+brew install asdf bat exa fd fish fzf gh httpie jq ripgrep wget
 $(which fish) -c 'curl -sL https://git.io/fisher | source && fisher update'
 
 # Add Homebrew fish to shells
